f81dfbb23995b1c70675baa832b05c7031adbcd9
Kevin <dukso123@gmail.com>
2024-01-24 00:59:53 +0900

    1. form.py: changed FlaskForm CreatePostForm()'s img_url is now not essential field

--- forms.py
+++ forms.py
@@ -14,7 +14,7 @@ db = SQLAlchemy()
 class CreatePostForm(FlaskForm):
     title = StringField("Blog Post Title", validators=[DataRequired()])
     subtitle = StringField("Subtitle", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Blog Image URL", validators=[URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
